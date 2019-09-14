75ea77f11b41f6658df42d8a81cd8ac7ab6818a2
AndyMu <andymu0601@gmail.com>
2019-09-14 15:04:36 +0800

    init

---
+++ objc-test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc-test
+//
+//  Created by AndyMu on 2019/9/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSObject *object = [NSObject alloc];
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
